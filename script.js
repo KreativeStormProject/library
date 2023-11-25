63a045ab1dd3f9b3320aa3c8a808b6c2503f046b
Clinton-stack <56035975+Clinton-stack@users.noreply.github.com>
2023-11-25 17:23:46 +0100

    cards and add button updated

--- script.js
+++ script.js
@@ -14,16 +14,64 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
-function validateForm() {
-    const inputs = document.forms["addBookModal"].elements;
 
-    for (let i = 0; i < inputs.length; i++) {
-        const input = inputs[i];
-        if (input.type !== "submit" && input.checkValidity && !input.checkValidity()) {
-            alert(input.validationMessage);
-            return false;
-        }
-    }
+function Book(title, author, numberP, read) {
+    this.title = title;
+    this.author = author;
+    this.numberP = numberP;
+    this.read = read === 'yes';
+  }
+  
+  let myLibrary = [];
+  
+  document.addEventListener('DOMContentLoaded', displayBooks);
+  
+  
+  function displayBooks() {
+    const container = document.getElementById('cards');
+    container.innerHTML = ''; // Clear existing content
+  
+    myLibrary.forEach((book, index) => {
+        const card = document.createElement('div');
+        card.className = 'card';
+  
+        card.innerHTML = `
+            <h2>Title: ${book.title}</h2>
+            <p>Author: ${book.author}</p>
+            <p>Number of pages: ${book.numberP}</p>
+            <p>Mark as Read
+                <label class="switch">
+                    <input type="checkbox" ${book.read ? 'checked' : ''}>
+                    <span class="slider round"></span>
+                </label>
+            </p>
+            <div class="icons">
+                <i class="material-icons" onclick="deleteBook(${index})">delete</i>
+            </div>
+        `;
+  
+        container.appendChild(card);
+    });
+    modal.style.display = 'none';
+  }
+  
 
-    return true;
-}
+  document.getElementById('addBookModal').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const numberP = document.getElementById('number_of_pages').value;
+    const read = document.getElementById('read').value;
+  
+    const newBook = new Book(title, author, numberP, read);
+    myLibrary.push(newBook);
+    displayBooks();
+  
+    document.getElementById('addBookModal').reset();
+  });
+  
+  function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+  }
